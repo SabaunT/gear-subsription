2cc6353b027e3ad7c00d4ee56c5554d077d92036
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-02-14 16:23:36 +0300

    Restructure, prepare for test

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use gear_subscription_io::SubscriptionMetadata;
+
 fn main() {
-    gear_wasm_builder::build();
+    gear_wasm_builder::build_with_metadata::<SubscriptionMetadata>();
 }
---
+++ state/src/lib.rs
@@ -0,0 +1,12 @@
+use gear_subscription_io::SubscriptionMetadata;
+use gmeta::{metawasm, Metadata};
+use gstd::ActorId;
+
+#[metawasm]
+pub trait Metawasm {
+    type State = <SubscriptionMetadata as Metadata>::State;
+
+    // fn subscribers(state: Self::State) -> Vec<ActorId> {
+    //     state.subscribers.clone()
+    // }
+}
